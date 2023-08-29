28d316d0a0a1a1f7d6ebcfd00cba643e5d7d10ea
Mayank <mayank.srmu@gmail.com>
2023-08-29 17:48:53 +0530

    Initial commit

---
+++ examples/nextjs/app/page.tsx
@@ -0,0 +1,9 @@
+import { ForkMe } from "@mayank1513/fork-me/server";
+
+export default function Page(): JSX.Element {
+	return (
+		<div>
+			<ForkMe href="https://github.com/mayank1513/turbo-template" />
+		</div>
+	);
+}
