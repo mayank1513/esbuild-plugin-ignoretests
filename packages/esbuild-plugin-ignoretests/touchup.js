f791c5a1b7c9644c86b62fb892ab9b0a896f001d
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-09-21 15:53:40 +0530

    create release during publish

--- packages/esbuild-plugin-ignoretests/touchup.js
+++ packages/esbuild-plugin-ignoretests/touchup.js
@@ -19,3 +19,35 @@ fs.copyFileSync(
 	path.resolve(__dirname, "..", "..", "README.md"),
 	path.resolve(__dirname, "dist", "README.md"),
 );
+
+if (process.env.TOKEN) {
+	const { Octokit } = require("octokit");
+	// Octokit.js
+	// https://github.com/octokit/core.js#readme
+	const octokit = new Octokit({
+		auth: process.env.TOKEN,
+	});
+
+	const octoOptions = {
+		owner: process.env.OWNER,
+		repo: process.env.REPO,
+		headers: {
+			"X-GitHub-Api-Version": "2022-11-28",
+		},
+	};
+	const tag_name = `v${packageJson.version}`;
+	const name = `Release ${tag_name}`;
+	/** Create a release */
+	octokit.request("POST /repos/{owner}/{repo}/releases", {
+		...octoOptions,
+		tag_name,
+		target_commitish: "main",
+		name,
+		draft: false,
+		prerelease: false,
+		generate_release_notes: true,
+		headers: {
+			"X-GitHub-Api-Version": "2022-11-28",
+		},
+	});
+}
